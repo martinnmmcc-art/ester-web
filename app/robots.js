81cb9b5978dc380183d74186807bd81545ea6f21
Martin Caceres <martinnm.mcc@gmail.com>
2026-09-14 23:31:15 -0300

    modo demo: noindex y canonical propio

--- app/robots.js
+++ app/robots.js
@@ -1,6 +1,9 @@
-import { SITE_URL } from "../lib/supabase";
+import { SITE_URL, MODO_DEMO } from "../lib/supabase";
 
 export default function robots() {
+  if (MODO_DEMO) {
+    return { rules: [{ userAgent: "*", disallow: "/" }] };
+  }
   return {
     rules: [{ userAgent: "*", allow: "/", disallow: ["/admin"] }],
     sitemap: SITE_URL + "/sitemap.xml"
